aa832bf21779143681e571dc589feb02376cc8a4
Wesley Lima <wesleyjoga500@gmail.com>
2020-05-05 10:38:48 -0300

    deploy

--- config/cors.js
+++ config/cors.js
@@ -16,7 +16,7 @@ module.exports = {
   | Function - Receives the current origin and should return one of the above values.
   |
   */
-  origin: ['https://sinos-frontend.herokuapp.com/'],
+  origin: ['https://sinos-frontend.herokuapp.com'],
 
   /*
   |--------------------------------------------------------------------------
